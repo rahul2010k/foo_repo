a347bd8f144721b087d0bd59d3095b17a27bf345
m1020157 <m1020157@D1ML05228.mindtree.com>
2012-12-18 14:22:30 +0530

    aASA

--- FooProj/src/com/rp/foo/test/FooTest.java
+++ FooProj/src/com/rp/foo/test/FooTest.java
@@ -6,12 +6,9 @@ public class FooTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
-
-		
-
 		System.out.println("Test2");
-
+		System.out.println("Test4");
+		System.out.println("Test5");
 		
 	}
 }
