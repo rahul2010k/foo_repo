926812fcbc9d5137c1a7b4dca9a434c505ecf6ad
m1020157 <m1020157@D1ML05228.mindtree.com>
2012-12-18 11:12:28 +0530

    Test

--- FooProj/src/com/rp/foo/test/FooTest.java
+++ FooProj/src/com/rp/foo/test/FooTest.java
@@ -8,5 +8,6 @@ public class FooTest {
 	public static void main(String[] args) {
 		System.out.print("This is test1...");
 		System.out.print("This is test2...");
+		System.out.print("This is test2...");
 	}
 }
